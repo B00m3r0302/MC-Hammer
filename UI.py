51f34ed0faf4a19608bf96a7f59d093441ad4a53
Woomer, Hudson [USA] <Woomer_Hudson@bah.com>
2023-10-23 11:34:32 -0400

    updated ui.py to be functional based on MaC-Hammer

--- UI.py
+++ UI.py
@@ -2,68 +2,74 @@ from blessed import Terminal
 from scanner import Scanner
 from view_tables import DatabaseViewer
 
-def display_menu_options(alert_count=0):
-    print("MC-Hammer Incident Detection and Response Tool")
-    print("---------------------------------------------")
-    print("1. Start Scan")
-    print("2. Stop Scan")
-    print("3. View Tables")
-    print("4. Add/Remove Trusted Connections")
-    print(f"5. View Alerts ({alert_count})")  # using f-string for variable substitution
-    print("6. Exit")
-    print("---------------------------------------------")
-    choice = input("Enter your choice: ")
-    return choice
+class App:
+    def __init__(self):
+        self.term = Terminal()
+        self.alerts = ["Alert 1: Suspicious activity detected", "Alert 2: High CPU usage"]
+        self.scanner = Scanner()
 
-def trusted_connection_option():
-    print("\n1. Add")
-    print("2. Remove")
-    choice = input("Do you want to add or remove a trusted connection? (Enter number): ")
+    def clear_screen(self):
+        print(self.term.clear())
 
-    if choice == "1":
-        ip_address = input("Enter the IP address to add: ")
-        # Logic to add the IP address to your data structure or database goes here
-        print(f"{ip_address} has been added!")
-    elif choice == "2":
-        id_number = input("Enter the ID of the trusted connection to remove: ")
-        # Logic to remove the trusted connection using the ID goes here
-        print(f"Trusted connection with ID {id_number} has been removed!")
-    else:
-        print("Invalid choice.")
+    def start_scan(self):
+        try:
+            # TODO: Make Sure this is the right function to call
+            self.scanner.scan_and_save()
+            print("Scan complete!")
+        except Exception as e:
+            print(f"An error occurred while starting scan: {str(e)}")
 
-def view_tables():
-    print("\n1. BaselineExecutables")
-    print("2. Baseline Accounts")
-    print("3. BaselineAutoRuns")
-    print("4. Discrepancies")
-    print("5. Current Connections")
-    print("6. Trusted Accounts")
-    choice = input("Which table would you like to see? (Enter number): ")
+    def view_tables(self):
+        self.clear_screen()
+        db_viewer = DatabaseViewer()
+        db_viewer.display_table_contents()
+        db_viewer.close_connection()
 
-    if choice == "6":
-        # This is a hardcoded example. In a real application, you'd retrieve this data from a database.
-        print("\nid     IPAddress\n1      127.0.0.1")
-    else:
-        # Placeholder for the other table views. Implement as needed.
-        print(f"Showing data for table option {choice}...")
+        input("\n Press Enter to continue...")
 
-def main():
-    while True:
-        alert_count = 0  # Example alert count. You can replace this with actual count.
-        choice = display_menu_options(alert_count)
+    def add_trusted_connection(self):
+        self.scanner.add_trusted_connection()
 
-        if choice == "3":
-            view_tables()
-        elif choice == "4":
-            trusted_connection_option()
-        elif choice == "6":
-            print("Exiting program...")
-            break
-        else:
-            # Placeholder for other functionalities (e.g., start scan, stop scan, etc.)
-            print(f"You chose option {choice}")
+    def remove_trusted_connection(self):
+        self.scanner.remove_trusted_connection()
 
-        input("\nPress Enter to continue...")  # This is to pause the loop and let user read the output
+    def alerts_menu(self):
+        self.clear_screen()
+
+        print("Alerts")
+        print("------")
+        for alert in self.alerts:
+            input("\n Press Enter to continue...")
+
+    def run(self):
+        while True:
+            print("MC-Hammer Incident Detection and Response Tool")
+            print("<= You Can't Touch This =>")
+            print("---------------------------------------------")
+            print("1. Start Scan")
+            print("2. View Tables")
+            print("3. Add Trusted Connections")
+            print("4. Remove Trusted Connection")
+            print(f"5. View Alerts ({alert_count})")  # using f-string for variable substitution
+            print("6. Exit")
+            print("---------------------------------------------")
+            
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                self.start_scan()
+            elif choice == "2":
+                self.view_tables()
+            elif choice == "3":
+                self.add_trusted_connection()
+            elif choice == "4":
+                self.remove_trusted_connection()
+            elif choice == "6":
+                self.clear_screen()
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.run()
